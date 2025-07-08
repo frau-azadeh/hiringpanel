c38e0c71ac0130e4a0e0f4a793ca2ff1ac110c5f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-08 23:01:21 +0330

    Remove unwanted files

--- Applicant.cs
+++
@@ -1,8 +0,0 @@
-public class Applicant
-{
-    public int Id { get; set; }
-    public string? FullName { get; set; }
-    public string? Email { get; set; }
-    public string? Phone { get; set; }
-    public string? Status { get; set; }
-}
